714de04dbeebae18b27140b1d2b7568d18253469
kafuuma <arkafuuma@gmail.com>
2020-06-07 16:53:37 -0400

    initial commit

---
+++ src/test/java/henry/springframework/petclininc/PetClinincApplicationTests.java
@@ -0,0 +1,13 @@
+package henry.springframework.petclininc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinincApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
